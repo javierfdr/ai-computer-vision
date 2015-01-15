bd266bb7f8937c5175f654f7c0e2e6429ac4801a
alejandroajhr <alejandro.ajhr@gmail.com>
2015-01-15 02:27:10 +0100

    added conf matrix and table

--- project3/part1/part1.m
+++ project3/part1/part1.m
@@ -12,32 +12,42 @@ Cs = [0.01, 0.1, 1, 2, 4]; % asked ones
 [models,Xtrain] = trainSvms('linear',100,Cs);
 
 % Saving Models
-save('SVMXtrain_100.mat','Xtrain');
-save('SVMmodels_100.mat','models');
+% save('SVMXtrain_100.mat','Xtrain');
+% save('SVMmodels_100.mat','models');
 
 % Loading Models
 % load('SVMXtrain_100.mat');
 % load('SVMmodels_100.mat');
 
 % Testing SVMs
-accMatrix = testSvms('linear',100,models,Xtrain,Cs)
-save('SVMaccMatrix_100','accMatrix');
+[accMatrix,confMatrix,confTable] = testSvms('linear',100,models,Xtrain,Cs);
+
+[~, bestcidx] = max(sum(accMatrix),[],2)
+bestC = Cs(bestcidx);
+bestAccs = accMatrix(:,bestcidx)
+bestConfMatrix = confMatrix(:,:,bestcidx)
+bestConfTable = confTable(:,:,bestcidx)
 
 %% 500 Examples
 % Training SVMs
 [models,Xtrain] = trainSvms('linear',500,Cs);
 
 % Saving Models
-save('SVMXtrain_500.mat','Xtrain');
-save('SVMmodels_500.mat','models');
+% save('SVMXtrain_500.mat','Xtrain');
+% save('SVMmodels_500.mat','models');
 
 % Loading Models
 % load('SVMXtrain_500.mat');
 % load('SVMmodels_500.mat');
 
 % Testing SVMs
-accMatrix = testSvms('linear',500,models,Xtrain,Cs)
-save('SVMaccMatrix_500','accMatrix');
+[accMatrix,confMatrix,confTable] = testSvms('linear',500,models,Xtrain,Cs);
+%%
+[~, bestcidx] = max(sum(accMatrix),[],2)
+bestC = Cs(bestcidx)
+bestAccs = accMatrix(:,bestcidx)
+bestConfMatrix = confMatrix(:,:,bestcidx)
+bestConfTable = confTable(:,:,bestcidx)
 
 %% 100 Examples and RBF
 %Cs = [1, 2, 4, 8, 16]; % good range if not standarized.
@@ -49,13 +59,21 @@ Gammas = [1e-2, 1e-3, 1e-4];
 [models,Xtrain] = trainSvms('rbf',100,Cs,Gammas);
 
 % Saving Models
-save('SVMXtrain_100_rbf.mat','Xtrain');
-save('SVMmodels_100_rbf.mat','models');
+% save('SVMXtrain_100_rbf.mat','Xtrain');
+% save('SVMmodels_100_rbf.mat','models');
 
 % Loading Models
 % load('SVMXtrain_100_rbf.mat');
 % load('SVMmodels_100_rbf.mat');
 
 % Testing SVMs
-accMatrix = testSvms('rbf',100,models,Xtrain,Cs,Gammas)
-save('SVMaccMatrix_100_rbf','accMatrix');
+[accMatrix,confMatrix,confTable] = testSvms('rbf',100,models,Xtrain,Cs,Gammas);
+
+[accsbyc, bestcidx] = max(sum(accMatrix),[],2);
+[~, bestgidx] = max(accsbyc,[],3)
+bestcidx = bestcidx(:,:,bestgidx)
+bestC = Cs(bestcidx)
+bestGamma = Gammas(bestgidx)
+bestAccs = accMatrix(:,bestcidx,bestgidx)
+bestConfMatrix = confMatrix(:,:,bestcidx,bestgidx)
+bestConfTable = confTable(:,:,bestcidx,bestgidx)
--- project3/part1/testSvms.m
+++ project3/part1/testSvms.m
@@ -2,7 +2,7 @@
 % @subject: Computer Vision
 % @studies: Master in Artificial Intelligence
 
-function [accMatrix] = testSvms(TYPE,SIZE,MODELS,XTRAIN,CS,GAMMAS)
+function [accMatrix,confMatrix,confTable] = testSvms(TYPE,SIZE,MODELS,XTRAIN,CS,GAMMAS)
     
     load('mnist_all.mat');
 
@@ -13,23 +13,30 @@ function [accMatrix] = testSvms(TYPE,SIZE,MODELS,XTRAIN,CS,GAMMAS)
     ytestMult = []; %Multiclass y
     for i=1:numel(testSets)
         setname = testSets{i};
+        setclass = eval(setname(end));
         Xtestfull = eval(setname);
         randidxs = randperm(length(Xtestfull));
         randidxs = randidxs(1:SIZE);
         Xtest = [Xtest, (double(Xtestfull(randidxs,:))/255)'];
-        ytestMult = [ytestMult, ones(1,SIZE)*eval(setname(end))];
+        ytestMult = [ytestMult, ones(1,SIZE)*setclass];
     end
         
     switch TYPE
       case 'linear'
         accMatrix = zeros(length(testSets),length(CS));
+        confMatrix = zeros(length(testSets),length(testSets),length(CS));
+        confTable = zeros(2,2,length(CS));
       case 'rbf'
         accMatrix = zeros(length(testSets),length(CS),length(GAMMAS));
+        confMatrix = zeros(length(testSets),length(testSets),length(CS),length(GAMMAS));
+        confTable = zeros(2,2,length(CS),length(GAMMAS));
     end
+    
     
     for i=1:numel(testSets)
         setname = testSets{i};
-        ytest = ytestMult == eval(setname(end));
+        setclass = eval(setname(end));
+        ytest = ytestMult == setclass;
         ytest = (ytest*2)-1; %to change 1s and 0s for 1s and -1s
         for c = 1:length(CS)
             if (strcmp(TYPE,'rbf'))
@@ -39,7 +46,19 @@ function [accMatrix] = testSvms(TYPE,SIZE,MODELS,XTRAIN,CS,GAMMAS)
                     b = MODELS{i,c,g}.b;
                     k_dist = L2_distance_sq(XTRAIN(:,svind),Xtest);
                     k = exp((- GAMMAS(g)) * k_dist);
-                    accMatrix(i,c,g) = sum(sign(alphay(svind)' * k + b) == ytest) / length(ytest);
+                    classification = sign(alphay(svind)' * k + b);
+                    accMatrix(i,c,g) = sum(classification == ytest) / length(ytest);
+                    
+                    for j=1:numel(testSets)
+                        setnamej = testSets{j};
+                        setclassj = eval(setnamej(end));
+                        confMatrix(i,j,c,g) = sum(classification(ytestMult == setclassj) == 1);
+                    end
+                    
+                    confTable(1,1,c,g) = confTable(1,1,c,g) + sum(classification(ytest==1)==1); %tp
+                    confTable(1,2,c,g) = confTable(1,2,c,g) + sum(classification(ytest==-1)==1); %fp
+                    confTable(2,1,c,g) = confTable(2,1,c,g) + sum(classification(ytest==1)==-1); %fn
+                    confTable(2,2,c,g) = confTable(2,2,c,g) + sum(classification(ytest==-1)==-1); %tn
                 end
             else
                 
@@ -48,7 +67,20 @@ function [accMatrix] = testSvms(TYPE,SIZE,MODELS,XTRAIN,CS,GAMMAS)
                 b = MODELS{i,c}.b;
                 k = XTRAIN(:,svind)'*Xtest;
                 % Testing all samples at once, vectorized
-                accMatrix(i,c) = sum(sign(alphay(svind)' * k + b) == ytest) / length(ytest);
+                classification = sign(alphay(svind)' * k + b);
+                accMatrix(i,c) = sum(classification== ytest) / length(ytest);
+                
+                for j=1:numel(testSets)
+                    setnamej = testSets{j};
+                    setclassj = eval(setnamej(end));
+                    confMatrix(i,j,c) = sum(classification(ytestMult == setclassj) == 1);
+                end
+                
+                confTable(1,1,c) = confTable(1,1,c) + sum(classification(ytest==1)==1); %tp
+                confTable(1,2,c) = confTable(1,2,c) + sum(classification(ytest==-1)==1); %fp
+                confTable(2,1,c) = confTable(2,1,c) + sum(classification(ytest==1)==-1); %fn
+                confTable(2,2,c) = confTable(2,2,c) + sum(classification(ytest==-1)==-1); %tn
+                
             end
             
     
